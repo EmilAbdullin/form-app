7fba5cc5a64c9d04d16d1ede164f326e589190cf
Emilio1111 <lyricstyle@bk.ru>
2020-01-07 16:04:05 +0300

    layout and some js files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-console.log('Here');
+import {isValid} from './utils'
+import './styles.css'
+
+
+const form = document.getElementById('question-form');
+const input = form.querySelector('#question-input');
+const buttonSubmit = form.querySelector('#submit-question');
+
+form.addEventListener('submit', submitFormHandler);
+input.addEventListener('input',()=>{
+    buttonSubmit.disabled = !isValid(input.value)
+})
+
+function submitFormHandler (event) {
+    event.preventDefault()
+
+    if( isValid(input.value) ) {
+        const question = {
+            text:input.value.trim(),
+            date: new Date().toJSON()
+        }
+        buttonSubmit.disabled = true
+        //Async request to server to save question
+        input.value = ''
+        input.className = ''
+        submitFormHandler.disabled = false
+    }
+}
